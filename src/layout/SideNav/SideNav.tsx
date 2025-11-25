a9796c5d0c73ca26f80a42d7c41e5f3f3742a77f
Petrovic Nikola (BD/SWD-WDE10) <pen3bg@bosch.com>
2025-11-25 18:44:40 +0100

    Test name

--- src/layout/SideNav/SideNav.tsx
+++ src/layout/SideNav/SideNav.tsx
@@ -4,16 +4,16 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { X, Menu } from "lucide-react";
 
-
 const NavItems = [
   { id: 1, label: "Dashboard", link: "/", icon: "home" },
   { id: 2, label: "Drafts", link: "/drafts", icon: "box-closed" },
-  {id: 3, label: "Available blocks", link: "/settings", icon: "wrench"}
-
+  { id: 3, label: "Available blocks", link: "/settings", icon: "wrench" },
 ];
 
 function SideNav() {
-  const [selectedNavItem, setSelectedNavItem] = useState<string>(NavItems[0].label);
+  const [selectedNavItem, setSelectedNavItem] = useState<string>(
+    NavItems[0].label
+  );
   const location = useLocation();
   useEffect(() => {
     const pathSegment = location.pathname.split("/")[1];
@@ -28,11 +28,13 @@ function SideNav() {
   }, [location]);
   const [isOpen, setIsOpen] = useState<boolean>(true);
 
-
   return (
-    <nav className={`side-nav ${isOpen ? "" : "closed"}`} aria-label="Main navigation">
+    <nav
+      className={`side-nav ${isOpen ? "" : "closed"}`}
+      aria-label="Main navigation"
+    >
       <div className="nav-header">
-        <span aria-label="BASS Application">TemplateX</span>
+        <span aria-label="BASS Application">TemplateXYC</span>
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="close-nav-icon"
