c1eeffedcef8ba2c4d6ba12d05a47cc5939694ac
zil1bg <zil1bg@bosch.com>
2025-11-26 18:27:13 +0100

    alert dialog refactor

--- src/components/blockCanvasComponents/EmailMetadataForm.tsx
+++ src/components/blockCanvasComponents/EmailMetadataForm.tsx
@@ -16,16 +16,9 @@ export const EmailMetadataForm = ({
 }: EmailMetadataFormProps) => {
   return (
     <div className="bg-card shadow border p-6">
-      <div className="mb-4">
-        <h2 className="text-base font-semibold text-foreground">
-          Master Template Metadata
-        </h2>
-        <p className="text-sm text-muted-foreground">
-          Subject line i preheader su sastavni deo master template-a.
-        </p>
-      </div>
-      <div className="grid gap-4 ">
-        <div className="flex flex-col gap-2">
+
+      <div className="flex gap-4 ">
+        <div className="flex flex-col gap-2 flex-1">
           <Label htmlFor="subject-line">Subject line</Label>
           <Input
             id="subject-line"
@@ -34,7 +27,7 @@ export const EmailMetadataForm = ({
             onChange={(event) => onSubjectLineChange(event.target.value)}
           />
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 flex-1">
           <Label htmlFor="preheader">Preheader</Label>
           <Input
             id="preheader"
